096dc42f0951bf42b475dfde57e6d0677b2278bd
Mahendra Shinde <MahendraShinde@synegertics-india.com>
2024-01-05 16:18:15 +0530

    Python env update

---
+++ demos/002-python-setup.md
@@ -0,0 +1,20 @@
+# Python Environment setup
+
+1. Download python for Windows
+2. Run the setup
+   1. Deselect TWO checkboxes 
+   2. Select "Customize Installation" or "Custom Installation"
+   3. Click NEXT
+   4. Deselect "install for all users"
+   5. Set the custom path for installation : C:\python3
+  
+> To set python in PATH Variable:
+
+    Start Menu > ENV > choose "Edit Env Vars for YOUR ACCOUNT"
+
+    Double click "PATH" Variable then click "new" button to add path "c:\python3"
+    Click "new" button once more and add path C:\python3\scripts
+
+
+To Upgrade PIP use
+python -m pip install --upgrade pip
--- demos/06-s3-python-demo/app.py
+++ demos/06-s3-python-demo/app.py
@@ -1,14 +1,18 @@
 import boto3
 
-s3 = boto3.resource('s3')
+s3 = boto3.resource('s3',region_name='us-east-1')
 
 for bucket in s3.buckets.all():
     print(bucket.name)
-    client.delete_bucket (Bucket=bucket.name)
-    
-response = s3.create_bucket(Bucket='abcd364687')
+
+response = s3.create_bucket(
+    Bucket='xyz1y2yt',
+    CreateBucketConfiguration={
+        'LocationConstraint': 'us-east-1',
+    },
+)
 print (response)
 
 ## Get the S3 client to perform CRUD on bucket objects
 client = boto3.client('s3')
-client.put_object(Bucket='abcd364687', Key='file101.txt', Body=b'Hello World')
+client.put_object(Bucket='xyz1y2yt', Key='file101.txt', Body=b'Hello World')
