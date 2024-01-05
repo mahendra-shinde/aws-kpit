66516e002d6abc24701d6e203414ffed6eb3d846
Mahendra Shinde <MahendraShinde@synegertics-india.com>
2024-01-05 15:27:35 +0530

    Creating a new bucket and uplaod file in python

--- demos/06-s3-python-demo/app.py
+++ demos/06-s3-python-demo/app.py
@@ -4,3 +4,11 @@ s3 = boto3.resource('s3')
 
 for bucket in s3.buckets.all():
     print(bucket.name)
+    client.delete_bucket (Bucket=bucket.name)
+    
+response = s3.create_bucket(Bucket='abcd364687')
+print (response)
+
+## Get the S3 client to perform CRUD on bucket objects
+client = boto3.client('s3')
+client.put_object(Bucket='abcd364687', Key='file101.txt', Body=b'Hello World')
