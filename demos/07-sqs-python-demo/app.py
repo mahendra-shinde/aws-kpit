fb1b0d518f02693bf309950dc77b8e9b19862715
Mahendra Shinde <MahendraShinde@synegertics-india.com>
2024-01-05 17:44:03 +0530

    send message from python

--- demos/07-sqs-python-demo/app.py
+++ demos/07-sqs-python-demo/app.py
@@ -1,8 +1,15 @@
 import boto3
 
+queueUrl="https://sqs.us-east-1.amazonaws.com/890756660068/orders"
 sqs = boto3.client('sqs')
 
-response = sqs.receive_message(QueueUrl="https://sqs.us-east-1.amazonaws.com/890756660068/orders")
+sqs.send_message(
+        QueueUrl=queueUrl,
+        MessageBody=('Hello Hinjawadi')
+)
+
+response = sqs.receive_message(QueueUrl=queueUrl)
 
 for message in response['Messages']:
     print (message['Body'])
+
