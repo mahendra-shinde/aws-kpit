07efe89d048f9dffd4fe475b2c362136bc69e58b
Mahendra Shinde <MahendraShinde@synegertics-india.com>
2024-01-05 14:46:36 +0530

    upload-s3-object

--- demos/s3-demo/src/main/java/com/mahendra/App.java
+++ demos/s3-demo/src/main/java/com/mahendra/App.java
@@ -1,12 +1,11 @@
 package com.mahendra;
 
-import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.Bucket;
-
 import java.util.List;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
@@ -19,5 +18,12 @@ public class App {
             System.out.println(b.getName());
         }
 
+        Scanner sc = new Scanner(System.in);
+
+        s3.createBucket("myfiles962476");
+        System.out.println("Enter the message: ");
+        String line = sc.nextLine();
+        s3.putObject("myfiles962476", "message.txt", line);
+        
     }
 }
