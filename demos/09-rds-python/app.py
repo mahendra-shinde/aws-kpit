08f639232c283b5d750c7131b62c9d96957b567a
Mahendra Shinde <MahendraShinde@synegertics-india.com>
2024-01-08 14:15:08 +0530

    RDS-Python-demo

---
+++ demos/09-rds-python/app.py
@@ -0,0 +1,41 @@
+## pip install mysql-connector-python
+
+import mysql.connector
+
+host = "sampledb.cimr0kdyqj64.us-east-1.rds.amazonaws.com"
+user = "admin"
+password = "Password!123"
+database = "sampledb"
+
+try:
+    connection = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database
+    )
+    if connection.is_connected():
+        print("Connected to the database")
+        # Create a cursor
+        cursor = connection.cursor()
+
+        # Execute SQL queries
+        cursor.execute("SELECT * FROM employees")
+        rows = cursor.fetchall()
+
+        # Process the result
+        for row in rows:
+            print(row)
+
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+
+finally:
+    # Close the cursor and connection
+    if 'cursor' in locals():
+        cursor.close()
+    if 'connection' in locals() and connection.is_connected():
+        connection.close()
+        print("Connection closed")
+
+
